aae32cd8f5018c26fe5a250b697e1131f75c0d2e
DefaltSimon <simon.peter.goricar@gmail.com>
2018-12-27 16:41:42 +0100

    Add rapidjson library and jsonify_response in utilities.py

--- soundcube/utilities.py
+++ soundcube/utilities.py
@@ -1,1 +1,15 @@
 # coding=utf-8
+from quart.wrappers.response import Response
+import logging
+
+log = logging.getLogger(__name__)
+
+try:
+    from rapidjson import dumps
+except ImportError:
+    log.info("'rapidjson' is not available, falling back to built-in json module.")
+    from json import dumps
+
+
+def jsonify_response(json, resp_code=200):
+    return Response(dumps(json), resp_code, mimetype="application/json")
