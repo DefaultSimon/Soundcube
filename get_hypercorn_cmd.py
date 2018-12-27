88919b714349d42ce3ff4656892f770165a52729
DefaltSimon <simon.peter.goricar@gmail.com>
2018-12-27 16:43:32 +0100

    Update get_hypercorn_cmd.py to include more options

--- get_hypercorn_cmd.py
+++ get_hypercorn_cmd.py
@@ -7,13 +7,24 @@
 
 from soundcube.config import config
 
+arg_list = []
+
+# Where the main file and app are located
 APP = config.get("Server", "app")
 
+# Which host:port to bind to
 HOST = config.get("Server", "host")
 PORT = config.get("Server", "port")
-binds = "{}:{}".format(HOST, PORT)
+arg_list.append("--bind {}:{}".format(HOST, PORT))
 
-command = "hypercorn --bind {binds} {app}".format(binds=binds, app=APP)
+# Whether to turn on the automatic reload for code changes
+RELOAD = config.getboolean("Server", "reload_on_change")
+if RELOAD is True:
+    arg_list.append("--reload")
 
-# Output the command
-print(command, flush=True, end="")
+
+# Add the app location at the end
+arg_list.append(APP)
+
+# Output the full command
+print("hypercorn " + " ".join(arg_list), flush=True, end="")
