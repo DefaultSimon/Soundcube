ec793f3ae2e9aa2252b59cf461843ad52b45566b
DefaltSimon <simon.peter.goricar@gmail.com>
2018-12-27 16:43:18 +0100

    Add .ini files for quart configuration
    First, the default config is loaded from soundcube/quartconfig_defaults.ini, then it is overwritten by user's custom settings (if present) in data/quartconfig.ini

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from quart import Quart
 
 app = Quart(__name__)
 
+# Load the default config and overwrite with custom one (if provided)
+app.config.from_pyfile("soundcube/quartconfig_defaults.ini")
+app.config.from_pyfile("data/quartconfig.ini", silent=True)
 
 @app.route('/')
 async def hello():
