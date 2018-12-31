4a72d3d30b274912a5dc4d18a8ea944e5d95dc14
DefaltSimon <simon.peter.goricar@gmail.com>
2018-12-31 18:36:18 +0100

    Update gitignore, get_hypercorn_cmd.py and api.raml

--- get_hypercorn_cmd.py
+++ get_hypercorn_cmd.py
@@ -18,14 +18,19 @@ PORT = config.get("Server", "port")
 arg_list.append("--bind {}:{}".format(HOST, PORT))
 
 # Whether to turn on the automatic reload for code changes
-RELOAD = config.getboolean("Server", "reload_on_change")
+RELOAD = config.getboolean("Server", "reload_on_change", fallback=False)
 if RELOAD is True:
     arg_list.append("--reload")
 
 # Whether to turn on debug mode
-DEBUG = config.getboolean("Server", "debug")
+DEBUG = config.getboolean("Server", "debug", fallback=False)
 if DEBUG is True:
     arg_list.append("--debug")
+
+# Include path to a toml config file if needed
+CONFIG_FILE = config.get("Server", "toml_config", fallback=None)
+if CONFIG_FILE:
+    arg_list.append(f"--config {CONFIG_FILE}")
 
 
 # Add the app location at the end
